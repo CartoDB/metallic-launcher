3e8b8a42cab77bc84747d75756380be1dd7a0d38
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-04-06 18:57:42 +0200

    Use spread operator to handle default values

--- src/cluster/server/server-factory.js
+++ src/cluster/server/server-factory.js
@@ -4,6 +4,10 @@ import Server from './server'
 
 export default class ServerFactory extends FactoryInterface {
   static create (metrics, logger, options) {
+    options = {
+      port: 0,
+      ...options
+    }
     const port = options.port
     const httpServer = HttpServerFactory.create(metrics, logger, { port })
     return new Server(httpServer, logger)
