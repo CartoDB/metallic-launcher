6c5ef07bcebea89beb56f08c807c3a6781461d82
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-27 20:06:43 +0100

    Fix test

--- test/unit/launcher-test.js
+++ test/unit/launcher-test.js
@@ -3,6 +3,8 @@ import sinon from 'sinon'
 import ClusterInterface from '../../src/cluster/cluster-interface'
 import LauncherInterface from '../../src/launcher-interface'
 import Launcher from '../../src/launcher'
+import { MetricsInterface } from 'metallic-metrics'
+import { LoggerInterface } from 'metallic-logger'
 
 class Target extends ClusterInterface {
   get role () {
@@ -10,13 +12,25 @@ class Target extends ClusterInterface {
   }
 }
 
+class Metrics extends MetricsInterface {
+  async run () {}
+  async close () {}
+}
+
+class Logger extends LoggerInterface {
+  async run () {}
+  async close () {}
+}
+
 describe('launcher', function () {
   beforeEach(function () {
     this.sandbox = sinon.sandbox.create()
 
     const target = this.target = new Target()
+    const metrics = this.metrics = new Metrics()
+    const logger = this.logger = new Logger()
 
-    this.launcher = new Launcher({ target })
+    this.launcher = new Launcher({ target, metrics, logger })
   })
 
   afterEach(function () {
@@ -33,39 +47,55 @@ describe('launcher', function () {
 
   it('.run() should init successfully', async function () {
     const targetRunStub = this.sandbox.stub(this.target, 'run').returns(Promise.resolve())
+    const metricsRunStub = this.sandbox.stub(this.metrics, 'run').returns(Promise.resolve())
+    const loggerRunStub = this.sandbox.stub(this.logger, 'run').returns(Promise.resolve())
 
     await this.launcher.run()
 
     assert.ok(targetRunStub.calledOnce)
+    assert.ok(metricsRunStub.calledOnce)
+    assert.ok(loggerRunStub.calledOnce)
   })
 
   it('.run() should throw error', async function () {
     const error = new Error('wadus')
+    const metricsRunStub = this.sandbox.stub(this.metrics, 'run').returns(Promise.resolve())
+    const loggerRunStub = this.sandbox.stub(this.logger, 'run').returns(Promise.resolve())
     const targetRunStub = this.sandbox.stub(this.target, 'run').returns(Promise.reject(error))
 
     try {
       await this.launcher.run()
     } catch (err) {
+      assert.ok(metricsRunStub.calledOnce)
+      assert.ok(loggerRunStub.calledOnce)
       assert.ok(targetRunStub.calledOnce)
       assert.equal(err, error)
     }
   })
 
   it('.close() should stop successfully', async function () {
+    const metricsRunStub = this.sandbox.stub(this.metrics, 'close').returns(Promise.resolve())
+    const loggerRunStub = this.sandbox.stub(this.logger, 'close').returns(Promise.resolve())
     const targetRunStub = this.sandbox.stub(this.target, 'close').returns(Promise.resolve())
 
     await this.launcher.close()
 
+    assert.ok(metricsRunStub.calledOnce)
+    assert.ok(loggerRunStub.calledOnce)
     assert.ok(targetRunStub.calledOnce)
   })
 
   it('.close() should throw error', async function () {
     const error = new Error('wadus')
+    const metricsRunStub = this.sandbox.stub(this.metrics, 'close').returns(Promise.resolve())
+    const loggerRunStub = this.sandbox.stub(this.logger, 'close').returns(Promise.resolve())
     const targetCloseStub = this.sandbox.stub(this.target, 'close').returns(Promise.reject(error))
 
     try {
       await this.launcher.close()
     } catch (err) {
+      assert.ok(metricsRunStub.calledOnce)
+      assert.ok(loggerRunStub.calledOnce)
       assert.ok(targetCloseStub.calledOnce)
       assert.equal(err, error)
     }
